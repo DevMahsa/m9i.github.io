ba57ce04661b3ca19052b22bc4b421dedc12fb95
=Mahsa <mahsa.gol89@gmail.com>
2020-01-30 01:04:05 +0330

    google-analyticas added

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-157313522-1`,
       },
     },
     `gatsby-plugin-feed`,
